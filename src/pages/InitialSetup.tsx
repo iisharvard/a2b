b268f696afe4cbf9b3f8849706656f55702c6700
Zilin Ma <zm003@bucknell.edu>
2025-03-12 23:27:13 -0400

    Fix API import errors in InitialSetup and ReviewAndRevise components

--- src/pages/InitialSetup.tsx
+++ src/pages/InitialSetup.tsx
@@ -26,7 +26,7 @@ import {
 } from '@mui/material';
 import { setCase, setParties, clearState, setCaseProcessed } from '../store/negotiationSlice';
 import LoadingOverlay from '../components/LoadingOverlay';
-import { api } from '../services/api.ts';
+import { api } from '../services/api';
 
 interface PartyOption {
   name: string;
